14d3da339adda0ea6f831031fa837ac76dcd41a5
Cartoon Kritthapath Yaviraj <krittapath6699@gmail.com>
2025-03-14 12:38:42 +0700

    Added Stuff Page

---
+++ src/routes/stuff/+page.svelte
@@ -0,0 +1,16 @@
+<div class="flex flex-col justify-center items-center space-x-2 space-y-2 p-4">
+    <div class="card bg-base-100 card-border border-base-300 shadow-lg w-full">
+        <div class="card-body">
+            <h1 class="font-bold card-title pb-4 justify-center items-center text-center">รวมลิงค์ของที่เคยซื้อทั้งหมด</h1>
+            <a class="btn bg-neutral text-white border-base-300" href="https://x.com/bbonbonblue" target="_blank" rel="noopener noreferrer">
+                ร้านชุดน้อนเอย ( ✿ bbonblue ✿ )
+            </a>
+
+            <div class="border-base-300 flex flex-col border-t py-2">
+                <a class="btn bg-error text-white border-base-300" href="/" rel="noopener noreferrer">
+                    <span class="font-bold">Back to Home | กลับหน้าหลัก</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
